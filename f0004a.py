5b344ba7cbb58d29c844e34aa9d7cb2bbd09aa70
botondzdenko <wmtech.zdenkobotond@gmail.com>
2020-11-02 13:40:30 +0000

    f0004a.py

--- f0004a.py
+++ f0004a.py
@@ -3,3 +3,5 @@ kor = input('És hány éves kend? ') # itt még stringet kapunk
 kor = int(kor) # átalakítjuk egész számmá, azaz int-té
 születési_év = 2016 - kor
 print(név, ', kend ', születési_év, '-ban született.', sep='')
+érettségi_év = születési_év + 18
+print(név, ',kend ', érettségi_év, '-ban érettsigézik.', sep='')
